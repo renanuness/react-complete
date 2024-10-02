c50a6cce6b5c3541b84203cf69815b507eac9af5
renanuness <renan1504@gmail.com>
2024-10-02 08:28:38 -0300

    first commit

---
+++ react-essentials/src/App.jsx
@@ -0,0 +1,36 @@
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcept';
+import { CORE_CONCEPTS } from './data';
+import TabButton from './components/TabButton';
+
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <main>
+        <section id="core-concepts">
+        <h2>Time to get started!</h2>
+        <ul>
+        <CoreConcept {...CORE_CONCEPTS[0]}/>
+        <CoreConcept {...CORE_CONCEPTS[1]}/>
+        <CoreConcept {...CORE_CONCEPTS[2]}/>
+        <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+        </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-essentials/src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+export default function TabButton(props){
+
+    return(
+        <li>
+            <button>
+                {props.children}
+            </button>
+        </li>
+    );
+}
